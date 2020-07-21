3c07ccd68ed96dec48ea19de921921902ba5abf3
Josh Campbell <jcampbell@zillowgroup.com>
2020-07-20 18:05:25 -0700

    initial commit

---
+++ src/brown_bear.py
@@ -0,0 +1,49 @@
+"""
+Brown Bear, Brown Bear
+by Bill Martin Jr and Eric Carle
+"""
+STORY = """
+    <amazon:emotion name="excited" intensity="low">
+    <prosody rate="80%" volume="soft">
+    <p>
+        Brown Bear, Brown Bear, What do you see?
+        I see a red bird looking at me.
+    </p>
+    <p>
+        Red Bird, Red Bird, What do you see?
+        I see a yellow duck looking at me.
+    </p>
+    <p>
+        Yellow Duck, Yellow Duck, What do you see?
+        I see a blue horse looking at me.
+    </p>
+    <p>
+        Blue Horse, Blue Horse, What do you see?
+        I see a green frog looking at me.
+    </p>
+    <p>
+        Green Frog, Green Frog, What do you see?
+        I see a purple cat looking at me.
+    </p>
+    <p>
+        Purple Cat, Purple Cat, What do you see?
+        I see a white dog looking at me.
+    </p>
+    <p>
+        White Dog, White Dog, What do you see?
+        I see a black sheep looking at me.
+    </p>
+    <p>
+        Black Sheep, Black Sheep, What do you see?
+        I see a goldfish looking at me.
+    </p>
+    <p>
+        Goldfish, Goldfish, What do you see?
+        I see a teacher looking at me.
+    </p>
+    <p>
+        Teacher, Teacher, What do you see?
+    </p>
+    </prosody>
+    </amazon:emotion>
+"""
